a6a3655241ecd5be5ee36c12550b97ece4c0be66
Nek-12 <vaizin.nikita@gmail.com>
2020-11-29 17:08:14 +0300

    added playground 1

---
+++ Playground_1/src/com/test/Main.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = Integer.MAX_VALUE;
+        System.out.println(a+1_000_000); //test overflow
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Character.MAX_VALUE);
+        char b = Byte.MAX_VALUE+1;
+        System.out.println((byte) b); //test casting
+        var  
+    }
+}
